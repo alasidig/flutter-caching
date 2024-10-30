1378a9552e94bffb99ed6d924bb96298ebb8fd6d
alasidig <ala.sidig@gmail.com>
2024-10-30 15:16:56 +0300

    shared preferences

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> storeData() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setString('Course', 'SWE463');
+}
+
+//get stored data
+Future<String?> getData() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getString('Course');
+}
+//clear stored data
+Future<void> clearData() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.remove('Course');
+}
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? _course = 'Press the read button to get the stored data';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Persistence'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Text(_course ?? 'No Stored data'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  getData().then((value) => _course = value);
+                });
+              },
+              child: const Text('Read'),
+            ),
+            const ElevatedButton(
+              onPressed: storeData,
+              child: Text('Save'),
+            ),const ElevatedButton(
+              onPressed: clearData,
+              child: Text('Clear'),
+            ),
+          ],
+        ),
+      ],),
+    );
+  }
+}
+
+
